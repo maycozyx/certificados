5ec2d87d76f2aa0b348e4851d8eb0398b560a5b8
Mayco <mayco.zyx@gmail.com>
2018-05-28 18:37:27 -0300

    Listando nomes

--- importa_planilha.php
+++ importa_planilha.php
@@ -7,17 +7,25 @@ ini_set('display_errors', 1 );
 	<title></title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<style type="text/css">
+		.break { page-break-before: always; }
+		.altura {
+			margin-top: 80mm;
+		}
 		p { 
 			text-align: center;
+			font-size: 28px;
+			font-weight: bold;
+			font-family: Verdana, Arial, Helvetica, sans-serif;
 		}
 	</style>
 </head>
-<body>
-<?php
+<body><?php
 $arq = file("../dados5s.csv");
 foreach ($arq as $temp_valor) { // c = coluna
       list( $c1 ) = explode(",", $temp_valor);
+	  echo "<div class='altura'></div>";
       echo "<p>" . $c1 . "</p>";
+	  echo "<br class='break'>";
 }
 
 ?>
